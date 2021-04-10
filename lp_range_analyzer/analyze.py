39a40750cc16f7a2d0dc02197040a136dd1f4d57
Martin Staadecker <machstg@gmail.com>
2021-04-09 21:40:41 -0400

    Drop unnecessary info from output

--- lp_range_analyzer/analyze.py
+++ lp_range_analyzer/analyze.py
@@ -3,104 +3,6 @@ Provides functions to analyze a Model.
 """
 from tabulate import tabulate
 
-show_full_rows = False
-
-
-def analyze_objective_range(model):
-    min_coef, max_coef = float('inf'), 0
-    min_var, max_var = None, None
-    for variable, coefficient in model.objective.coefficients.items():
-        abs_coefficient = abs(coefficient)
-        if abs_coefficient < min_coef:
-            min_coef = abs_coefficient
-            min_var = variable
-        if abs_coefficient > max_coef:
-            max_coef = abs_coefficient
-            max_var = variable
-
-    if show_full_rows:
-        print("Full objective is: ", end="")
-        model.objective.print()
-
-    return [["Min Objective", min_coef, None, min_var],
-            ["Max Objective", None, max_coef, max_var]]
-
-
-def analyze_coefficient_range(model):
-    min_coef, max_coef = float('inf'), 0
-    min_row, max_row = None, None
-    min_var, max_var = None, None
-    for row in model.rows.values():
-        if model.objective == row:
-            continue
-        for variable, coefficient in row.coefficients.items():
-            abs_coefficient = abs(coefficient)
-            if abs_coefficient < min_coef:
-                min_coef = abs_coefficient
-                min_row = row
-                min_var = variable
-            if abs_coefficient > max_coef:
-                max_coef = abs_coefficient
-                max_row = row
-                max_var = variable
-
-    if show_full_rows:
-        print("Full max row is: ", end="")
-        max_row.print()
-        print("Full min row is: ", end="")
-        min_row.print()
-
-    return [["Min Coef", min_coef, None, min_var],
-            ["Max Coef", None, max_coef, max_var]]
-
-
-def anaylze_rhs(model):
-    min_rhs, max_rhs = float('inf'), 0
-    min_row, max_row = None, None
-    for row in model.rows.values():
-        if row.rhs_value is None or row.rhs_value == 0.0:
-            continue
-        abs_rhs = abs(row.rhs_value)
-        if abs_rhs < min_rhs:
-            min_rhs = abs_rhs
-            min_row = row
-        if abs_rhs > max_rhs:
-            max_rhs = abs_rhs
-            max_row = row
-
-    if show_full_rows:
-        print("Full max rhs row is: ", end="")
-        max_row.print()
-        print("Full min rhs row is: ", end="")
-        min_row.print()
-
-    return [["RHS min", min_rhs, None, min_row.row_name], ["RHS Max", None, max_rhs, max_row.row_name]]
-
-
-def anaylze_bounds(model):
-    min_bound_value, max_bound_value = float('inf'), 0
-    min_bound, max_bound = None, None
-    for bounds in model.bounds.values():
-        for bound in (bounds.lhs_bound, bounds.rhs_bound):
-            if bound is None:
-                continue
-            abs_bound = abs(bound)
-            if abs_bound < min_bound_value:
-                min_bound_value = abs_bound
-                min_bound = bounds
-            if abs_bound > max_bound_value:
-                max_bound_value = abs_bound
-                max_bound = bounds
-
-    if show_full_rows:
-        print("Full max bound row is: ", end="")
-        max_bound.print()
-        print("Full min bound row is: ", end="")
-        min_bound.print()
-
-    return [["Min Bound", min_bound.lhs_bound, min_bound.rhs_bound, min_bound.name],
-            ["Max Bound", max_bound.lhs_bound, max_bound.rhs_bound, max_bound.name]]
-
 
 class AnalyzeVariable:
     def __init__(self, name):
@@ -116,6 +18,18 @@ class AnalyzeVariable:
 
     def get_table_row(self):
         return [self.name, self.min_coef, self.max_coef, self.min_bound, self.max_bound]
+
+
+class AnalyzeRow:
+    def __init__(self, name):
+        self.name = name
+        self.min_rhs = float('inf')
+        self.max_rhs = 0
+        self.min_coef = float('inf')
+        self.max_coef = 0
+
+    def get_table_row(self):
+        return [self.name, self.min_coef, self.max_coef, self.min_rhs, self.max_rhs]
 
 
 def analyze_by_variable_type(model):
@@ -155,17 +69,6 @@ def analyze_by_variable_type(model):
                  floatfmt=".2")
     return out
 
-class AnalyzeRow:
-    def __init__(self, name):
-        self.name = name
-        self.min_rhs = float('inf')
-        self.max_rhs = 0
-        self.min_coef = float('inf')
-        self.max_coef = 0
-
-    def get_table_row(self):
-        return [self.name, self.min_coef, self.max_coef, self.min_rhs, self.max_rhs]
-
 def analyze_by_row_type(model):
     all_rows = {}
     for row in model.rows.values():
@@ -193,18 +96,8 @@ def analyze_by_row_type(model):
 
 
 def full_analysis(model, outfile):
-    # Analyze the main groups
-    table = []
-    table.extend(analyze_objective_range(model))
-    table.extend(analyze_coefficient_range(model))
-    table.extend(anaylze_rhs(model))
-    table.extend(anaylze_bounds(model))
-
-    output = tabulate(table, headers=["Type", "Min", "Max", "Var"], tablefmt="github", floatfmt=".2")
-    output += "\n\n"
-
     # Analyze by variable
-    output += analyze_by_variable_type(model)
+    output = analyze_by_variable_type(model)
     output += "\n\n"
 
     # Analyze by row type
